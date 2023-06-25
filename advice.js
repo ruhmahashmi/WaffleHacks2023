76a3d26ccefa440f25434624b6c089a78da8d3c7
ruhmahashmi <86506826+ruhmahashmi@users.noreply.github.com>
2023-06-25 14:56:35 +0000

    update 2.0

--- advice.js
+++ advice.js
@@ -32,24 +32,24 @@ function openModal(index) {
   const adviceContent = [
     `<h3>Smart Meal Planning</h3>
     <div class="advice-image">
-      <img src="meal-planning-image.jpg" alt="Smart Meal Planning">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8F-4W8lAGtdcXmQGb_zp-aU6RzKbVeiClZA&usqp=CAU" alt="Smart Meal Planning">
     </div>
     <div class="advice-content">
       <p style="text-align: justify;">Smart meal planning can be helpful to overcome food insecurity as a student!<br> By planning your meals in advance, you can avoid impulse purchases and ensure that you have the necessary ingredients on hand.</p>
       <p>Consider the following tips:</p>
       <ul>
-        <li><strong>Plan your meals:</strong> <br>Take some time each week to plan your meals. This will help you create a shopping list and avoid buying unnecessary items. <p> Check out <a href="https://www.examplemealplanningwebsite.com">Example Meal Planning Website</a> for meal planning templates and ideas.</p></li>
+        <li><strong>Plan your meals:</strong> <br>Take some time each week to plan your meals. This will help you create a shopping list and avoid buying unnecessary items. <p> Check out <a href="https://www.adobe.com/express/create/planner/meal"> this Meal Planning Website</a> for meal planning templates and ideas.</p></li>
         <br>
-        <li><strong>Buy in bulk:</strong> <br>Purchase staple ingredients in larger quantities to save money in the long run. Items like rice, pasta, beans, and canned goods can be stored and used over time.<p> Visit <a href="https://www.examplebulkbuyingwebsite.com">Example Bulk Buying Website</a> for bulk purchasing options and discounts.</p></li>
+        <li><strong>Buy in bulk:</strong> <br>Purchase staple ingredients in larger quantities to save money in the long run. Items like rice, pasta, beans, and canned goods can be stored and used over time.<p> Visit <a href="https://bulkfoodbox.com/">this Bulk Buying Website</a> for bulk purchasing options and discounts.</p></li>
         <br>
-        <li><strong>Utilize leftovers:</strong> <br> Get creative with leftovers by repurposing them into new dishes. For example, leftover roasted chicken can be used in sandwiches or salads, and extra cooked vegetables can be turned into flavorful stir-fries or omelettes. <p> Check out <a href="https://www.exampleleftoverrecipeswebsite.com">Example Leftover Recipes Website</a> for inspiration.</p> </li>
+        <li><strong>Utilize leftovers:</strong> <br> Get creative with leftovers by repurposing them into new dishes. For example, leftover roasted chicken can be used in sandwiches or salads, and extra cooked vegetables can be turned into flavorful stir-fries or omelettes. <p> Check out <a href="https://no-leftovers.com/">this Leftover Recipes Website</a> for inspiration.</p> </li>
       </ul>
       <p style="text-align: justify;">Remember, smart meal planning not only helps with budgeting but also ensures that you have nutritious meals throughout the week.</p>
     </div>`,
     
     `
 <div class="advice-section">
-  <h2>Budget-Friendly Meals: Delicious and Affordable Recipes</h2>
+  <h2>Budget-Friendly Meals: Delicious and Affordable Ideas</h2>
   <div class="advice-container">
     <div class="advice-column" onclick="openModal(5)">
       <h3>5-Ingredient Wonders</h3>
@@ -89,22 +89,22 @@ function openModal(index) {
 <section id="cooking-tips">
   <h2>Cooking Tips and Tricks</h2>
   <div class="tip-card">
-    <img src="images/meal-planning.jpg" alt="Meal Planning">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAg85bQ8KyYMyhteJmlYvbJFi11Bz5ZtCfjQ&usqp=CAU" alt="Meal Planning">
     <h3>One-pot meals</h3>
     <p>Explore recipes that require minimal equipment and can be cooked in a single pot or pan. This reduces the need for multiple cooking utensils and saves time on cleaning.</p>
   </div>
   <div class="tip-card">
-    <img src="images/budget-friendly.jpg" alt="Budget-Friendly Ingredients">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD_cG3vQLKto36I2_zm7ltt11-nh_En9PD2A&usqp=CAU" alt="Budget-Friendly Ingredients">
     <h3>Budget-Friendly Ingredients</h3>
     <p>Opt for affordable ingredients like rice, beans, and frozen vegetables to save money.These items are often inexpensive, versatile, and have a long shelf life. Invest in a few basic spices and herbs to add flavor to your meals. This can make even simple dishes more enjoyable without the need for expensive sauces or seasonings.</p>
   </div>
   <div class="tip-card">
-    <img src="images/bulk-cooking.jpg" alt="Bulk Cooking">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiui9CaCEqlhD7-hYLu6IQWvq11xhRr87q7w&usqp=CAU" alt="Bulk Cooking">
     <h3>DIY snacks</h3>
     <p>Make your own snacks instead of buying pre-packaged ones. For example, bake homemade granola bars, prepare trail mix with nuts and dried fruits, or create your own popcorn flavors using kernels and spices.</p>
   </div>
   <div class="tip-card">
-    <img src="images/repurpose-leftovers.jpg" alt="Repurpose Leftovers">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVVGlG3IJOQFUdedpZ1640jMNqlMoBVWT1rg&usqp=CAU" alt="Repurpose Leftovers">
     <h3>Repurpose Leftovers</h3>
     <p>Get creative with repurposing leftovers. Turn yesterday's roasted chicken into a sandwich filling, transform cooked rice into a flavorful fried rice, or use extra veggies in omelets, stir-fries, or wraps.</p>
   </div>
--- planner.js
+++ planner.js
@@ -1,87 +1,25 @@
-// Weekly meal plan data
-let mealPlan = [
-  { day: 'Monday', meal: 'Breakfast', recipe: 'Omelette' },
-  { day: 'Monday', meal: 'Lunch', recipe: 'Grilled Chicken Salad' },
-  { day: 'Monday', meal: 'Dinner', recipe: 'Spaghetti Bolognese' },
-  { day: 'Tuesday', meal: 'Breakfast', recipe: 'Yogurt with Fruits' },
-  { day: 'Tuesday', meal: 'Lunch', recipe: 'Vegetable Stir-Fry' },
-  { day: 'Tuesday', meal: 'Dinner', recipe: 'Baked Salmon with Veggies' },
-  // Add more meal plan data for the remaining days of the week
-];
+// JavaScript for To-Do List
 
-// Grocery list data
-let groceryList = [
-  'Eggs',
-  'Chicken',
-  'Lettuce',
-  'Tomatoes',
-  'Pasta',
-  'Ground beef',
-  'Yogurt',
-  'Assorted fruits',
-  'Assorted vegetables',
-  'Salmon fillets',
-  // Add more grocery items as needed
-];
+// Selecting elements
+const todoInput = document.getElementById("todo-input");
+const todoButton = document.getElementById("todo-button");
+const todoItems = document.getElementById("to-do-items");
 
-// To-Do list data
-let toDoList = [];
+// Adding event listener to the "Add" button
+todoButton.addEventListener("click", addTodo);
 
-// Function to render the meal plan
-function renderMealPlan() {
-  const mealPlanContainer = document.getElementById('meal-plan');
-  mealPlanContainer.innerHTML = ''; // Clear existing meal plan
+// Function to add a new task
+function addTodo() {
+  // Get the value of the input field
+  const todoText = todoInput.value;
 
-  mealPlan.forEach((item) => {
-    const mealItem = document.createElement('div');
-    mealItem.innerHTML = `<strong>${item.day} - ${item.meal}:</strong> ${item.recipe}`;
-    mealPlanContainer.appendChild(mealItem);
-  });
+  // Create a new list item element
+  const li = document.createElement("li");
+  li.innerText = todoText;
+
+  // Append the new list item to the to-do items list
+  todoItems.appendChild(li);
+
+  // Clear the input field
+  todoInput.value = "";
 }
-
-// Function to render the grocery list
-function renderGroceryList() {
-  const groceryItemsContainer = document.getElementById('grocery-items');
-  groceryItemsContainer.innerHTML = ''; // Clear existing grocery list
-
-  groceryList.forEach((item) => {
-    const groceryItem = document.createElement('li');
-    groceryItem.textContent = item;
-    groceryItemsContainer.appendChild(groceryItem);
-  });
-}
-
-// Function to render the to-do list
-function renderToDoList() {
-  const toDoItemsContainer = document.getElementById('to-do-items');
-  toDoItemsContainer.innerHTML = ''; // Clear existing to-do list
-
-  toDoList.forEach((item) => {
-    const toDoItem = document.createElement('li');
-    toDoItem.textContent = item;
-    toDoItemsContainer.appendChild(toDoItem);
-  });
-}
-
-// Function to add a task to the to-do list
-function addTask() {
-  const taskInput = document.getElementById('todo-input');
-  const task = taskInput.value.trim();
-
-  if (task !== '') {
-    toDoList.push(task);
-    taskInput.value = '';
-    renderToDoList();
-  }
-}
-
-// Initialize the meal planner
-document.addEventListener('DOMContentLoaded', () => {
-  renderMealPlan();
-  renderGroceryList();
-  renderToDoList();
-
-  // Add event listener for the "Add" button in the to-do list
-  const addTodoButton = document.getElementById('todo-button');
-  addTodoButton.addEventListener('click', addTask);
-});
